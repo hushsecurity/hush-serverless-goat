8f7470501d6edcfe199f1dc46e060a253801ee08
Gilad Sever <gilad@hush.security>
2025-12-17 17:48:56 +0200

    HUSH-4119 main.tf: Add lambda logs

--- main.tf
+++ main.tf
@@ -39,6 +39,26 @@ resource "aws_iam_role_policy" "lambda_secret_policy" {
   })
 }
 
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name              = "/aws/lambda/hush_goat_lambda"
+  retention_in_days = 3
+}
+
+resource "aws_iam_role_policy" "lambda_logs_policy" {
+  role = aws_iam_role.iam_for_lambda.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+      Effect   = "Allow"
+      Resource = "${aws_cloudwatch_log_group.lambda_log_group.arn}:*"
+    }]
+  })
+}
+
 # The Lambda Function
 resource "aws_lambda_function" "lambda" {
   filename         = "build/lambda.zip"
@@ -48,7 +68,9 @@ resource "aws_lambda_function" "lambda" {
   runtime          = "python3.12"
   source_code_hash = filebase64sha256("build/lambda.zip")
 
+  depends_on = [aws_cloudwatch_log_group.lambda_log_group]
+
   lifecycle {
-    ignore_changes = [environment]
+    ignore_changes = [environment, layers]
   }
 }
